fbb8458e0b4ed8f80e38d3257ebed92335cdd53e
hadssb1289 <hbandela1289@gmail.com>
2024-07-08 22:11:25 +0530

    Time: 45 ms (29.95%) | Memory: 44.7 MB (65.44%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution found");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        System.out.print("Enter the target value: ");
+        int target = scanner.nextInt();
+        try {
+            int[] result = twoSum(nums, target);
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+       
